b10f545e81d4440f6f6c79c11e776196dae7d150
BobDylans <119731046+BobDylans@users.noreply.github.com>
2023-03-31 13:48:51 +0800

    程子豪的第8次提交

--- src/main/java/com/rental/premisesrental/service/impl/OrderServiceImpl.java
+++ src/main/java/com/rental/premisesrental/service/impl/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package com.rental.premisesrental.service.impl;
 
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.rental.premisesrental.config.RedissonConfig;
 import com.rental.premisesrental.entity.Order;
 import com.rental.premisesrental.entity.Place;
 import com.rental.premisesrental.mapper.OrderMapper;
@@ -10,11 +12,15 @@ import com.rental.premisesrental.service.OrderService;
 import com.rental.premisesrental.util.AvailableTimeUtil;
 import com.rental.premisesrental.util.Response;
 import com.rental.premisesrental.util.UserHolder;
+import io.lettuce.core.RedisClient;
 import lombok.extern.java.Log;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -23,27 +29,43 @@ import java.util.List;
  */
 @Service
 @Transactional
-public class OrderServiceImpl implements OrderService {
+public class OrderServiceImpl extends ServiceImpl<OrderMapper,Order> implements OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
 
     @Autowired
     private PlaceMapper placeMapper;
+    @Resource
+    private RedissonClient redisClient;
 
 
     @Override
     public Response createOrder(Long shopId, Long placeId, Integer dayOffSet, Integer beginTime, Integer rentTime) {
+        Long userId = UserHolder.getCurrentUser().getId();
+
+        //创建锁对象
+        RLock redisLock = redisClient.getLock("lock:order:" + userId);
+        //尝试获取锁
+        boolean isLock = redisLock.tryLock();
+        if (!isLock){
+            //获取锁失败,返回失败信息
+            return Response.fail().setFailMessage("不允许重复下单!");
+        }
+
+
         Timestamp beginRentTime = AvailableTimeUtil.getFinalDate(dayOffSet);
         Order order = new Order(
-                UserHolder.getCurrentUser().getId(),
+                userId,
                 shopId,
                 placeId,
                 beginRentTime,
                 rentTime,
                 new Timestamp(System.currentTimeMillis())
         );
+
         orderMapper.insert(order);
+
         LambdaQueryWrapper<Place> placeLambdaQueryWrapper = new LambdaQueryWrapper<>();
         placeLambdaQueryWrapper.eq(Place::getId, placeId);
         Place place = placeMapper.selectOne(placeLambdaQueryWrapper);
@@ -51,6 +73,7 @@ public class OrderServiceImpl implements OrderService {
         List<Long> longs = JSON.parseArray(place.getAvailable()).toList(Long.class);
          // 取出一个列表
         Long day = longs.get(dayOffSet);
+
 
         long tem = 1;
         tem <<= (24 - beginTime - 1);
